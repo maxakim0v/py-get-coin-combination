d59037b22537db64eb4379b281ad8000ad2cd497
Max <makimov2004@gmail.com>
2024-12-19 00:37:36 +0200

    Fix

--- app/test_main.py
+++ app/test_main.py
@@ -2,21 +2,22 @@ import pytest
 from app.main import get_coin_combination
 
 
+# write your tests here
 @pytest.mark.parametrize(
-    "cents, expected", [
+    "input_cents, result",
+    [
+        (0, [0, 0, 0, 0]),
         (1, [1, 0, 0, 0]),
-        (6, [1, 1, 0, 0]),
-        (17, [2, 1, 1, 0]),
-        (50, [0, 0, 0, 2]),
-        (99, [4, 0, 2, 3]),
-        (100, [0, 0, 0, 4]),
-        (24, [4, 1, 1, 0]),
-        (3, [3, 0, 0, 0]),
         (5, [0, 1, 0, 0]),
-        (30, [0, 1, 0, 1]),
-        (0, [0, 0, 0, 0])
-
+        (6, [1, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (16, [1, 1, 1, 0]),
+        (17, [2, 1, 1, 0]),
+        (25, [0, 0, 0, 1]),
+        (41, [1, 1, 1, 1]),
+        (50, [0, 0, 0, 2]),
+        (106, [1, 1, 0, 4]),
     ]
 )
-def test_get_coin_combination(cents, expected):
-    assert get_coin_combination(cents) == expected
+def test_get_coin_combination(input_cents: int, result: list) -> None:
+    assert get_coin_combination(input_cents) == result
